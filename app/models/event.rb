439786d800cccd52187661a291410686eb46e163
Liam Andrew <landrew@mit.edu>
2014-05-20 10:35:48 -0400

    Push actually-related articles to the 'related articles' list

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,6 +11,17 @@ class Event < ActiveRecord::Base
   end
 
   def related_events
-    Event.where('id != ?', id).newest(5)
+    related_events = []
+    entities.each do |entity|
+      entity.genres.includes(:taggings).where("taggings_count > 1").each do |genre|
+        entity_ids = genre.taggings.where("taggable_id != ?", entity.id).pluck('taggable_id')
+        Entity.where(id: entity_ids).each do |related_entity|
+          related_entity.events.each do |related_event|
+            related_events.push(related_event)
+          end
+        end
+      end
+    end
+    return related_events[0..4]
   end
 end
