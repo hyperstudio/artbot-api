5b7cd8c555adcc80664b9449d9c024706208516b
Dan Collis-Puro <djcp@thoughtbot.com>
2014-08-14 16:46:53 -0400

    Remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :possible_interests, only: [:index]
   resources :interests, only: [:index, :create, :destroy]
 
-  resources :favorites, only: [:index, :show, :destroy, :update] do
+  resources :favorites, only: [:index, :destroy, :update] do
     collection do
       get 'history'
     end
