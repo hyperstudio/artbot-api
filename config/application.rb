44ede499d24158ace88af0f6f925457e2d4bf7df
Jamie Folsom <jamiefolsom@gmail.com>
2014-08-14 12:47:46 -0400

    WIP Cors config, with examples. Pushing to staging to test CORS.

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Artx
     config.middleware.use Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
 
