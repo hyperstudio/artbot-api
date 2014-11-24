0646e6699c813eb1269f663552c50a2e739c3648
Liam Andrew <landrew@mit.edu>
2014-11-24 14:45:11 -0500

    Fix initializer for test env

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,19 +1,17 @@
 # Put dev images in a separate folder so they don't overlap
 if Rails.env.test?
-    startpath = '#{Rails.root}/spec/test_files/'
-elsif Rails.env.development?
-    startpath = 'dev/'
+    Paperclip::Attachment.default_options[:path] = '#{Rails.root}/spec/test_files/:class/:id.:style.:extension'
 else
     startpath = ''
+    startpath += 'dev/' if Rails.env.development?
+    Paperclip::Attachment.default_options.merge!({
+        :storage => :s3,
+        :url => ':s3_domain_url',
+        :path => startpath + ':class/:id.:style.:extension',
+        :s3_credentials => {
+            :bucket => ENV['S3_BUCKET_NAME'],
+            :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+        }
+    })
 end
-
-Paperclip::Attachment.default_options.merge!({
-    :storage => :s3,
-    :url => ':s3_domain_url',
-    :path => startpath + ':class/:id.:style.:extension',
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-})
